82a511a9c3a5d77192b7f3716c8c009863b9eec4
haionaz <samhsj@naver.com>
2025-01-03 23:37:25 +0900

    feat: Prompt 수정, 에러 처리, 반환 타입 json object로 변경

--- uner-express/src/index.ts
+++ uner-express/src/index.ts
@@ -21,9 +21,15 @@ app.get('/problems', (req, res) => {
 
 app.get('/prompt', (req, res) => {
   const { givenWord, userPrompt } = req.query;
-  // const givenWord = 'grandiloquent';
-  // const userPrompt = '허풍스러운';
-  // const notCorrectPrompt = '호화로운';
+
+  if (!givenWord) {
+    res.status(400).json({ message: '단어가 주어지지 않았습니다' });
+    return;
+  }
+  if (!userPrompt) {
+    res.status(400).json({ message: '유저 답변이 주어지지 않았습니다' });
+    return;
+  }
 
   const prompt = `Given Word : ${givenWord}, User's Answer : ${userPrompt}`;
 
@@ -36,7 +42,10 @@ app.get('/prompt', (req, res) => {
         content: [
           {
             type: 'text',
-            text: '당신은 한국인 학생을 가르치는 영어 교사입니다. 주어진 영어 단어와 비교해서 유저가 제시하는 한국어 단어의 뜻을 판단하고, 적절한 한글 뜻이 제시되었는지 판별해주세요. 첫 줄은 O / X로 적합성 여부를 판별해주세요. 두번째 줄부터 이에 대한 부연 설명을 한국어로 작성해주세요.',
+            text: `당신은 한국인 학생을 가르치는 영어 교사입니다. 주어진 영어 단어와 비교해서 유저가 제시하는 한국어 단어의 뜻을 판단하고, 적절한 한글 뜻이 제시되었는지 판별해주세요. 
+            답변의 json_object 스키마는 다음과 같습니다.
+            is_answer로는 true / false boolean의 타입으로 적합성 여부를 판별해주세요.
+            description으로는 이에 대한 부연 설명을 한국어로 작성해주세요.`,
           },
         ],
       },
@@ -46,7 +55,7 @@ app.get('/prompt', (req, res) => {
       },
     ],
     response_format: {
-      type: 'text',
+      type: 'json_object',
     },
     temperature: 0.8,
     max_completion_tokens: 256,
@@ -55,7 +64,14 @@ app.get('/prompt', (req, res) => {
     presence_penalty: 0,
   });
 
-  response.then(result => res.json(result.choices[0].message.content));
+  response.then(result => {
+    const answer = result.choices[0].message.content;
+    if (answer === null) {
+      res.status(400).json({ message: 'GPT가 응답하지 않습니다' });
+      return;
+    }
+    res.json(JSON.parse(answer || ''));
+  });
 });
 
 app.listen(PORT, () => {
