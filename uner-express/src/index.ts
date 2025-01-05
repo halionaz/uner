e62b74ce8512750b62a20fcc024a7202d3a2cec8
haionaz <samhsj@naver.com>
2025-01-06 00:46:46 +0900

    fix: 프롬프트 수정, 유사 단어 받지 않음

--- uner-express/src/index.ts
+++ uner-express/src/index.ts
@@ -62,6 +62,16 @@ app.get('/eng-to-kor/words', (req, res) => {
       difficulty: 2,
       topic: [],
     },
+    {
+      id: 5,
+      english: 'gift',
+      definitions: [
+        { definition: '선물', partOfSpeech: 'noun' },
+        { definition: '재능', partOfSpeech: 'noun' },
+      ],
+      difficulty: 1,
+      topic: [],
+    },
   ];
   res.status(200).json({
     words,
@@ -92,9 +102,9 @@ app.get('/eng-to-kor/answer', (req, res) => {
           {
             type: 'text',
             text: `당신은 한국인 학생을 가르치는 영어 교사에요. 주어진 영어 단어와 비교해서 유저가 제시하는 한국어 단어의 뜻을 판단하고, 적절한 한글 뜻이 제시되었는지 판별해주세요. 
-            답변의 json_object 스키마는 다음과 같아요.
-            is_answer로는 true / false boolean의 타입으로 유저가 단어의 뜻을 올바르게 해석했는지 적합성 여부를 판별해주세요. 여러 의미를 가진 단어라면 그 중 하나의 뜻만 제시되어도 정답 처리해주세요. 의미가 맞더라도 품사가 다르다면 오답 처리해주세요.
-            description으로는 여러 의미를 가진 단어일 때 유저가 제시한 답이 아닌 다른 뜻들을 문장으로 제시해주세요.`,
+            답변의 json_object 스키마는 boolean 타입의 is_answer와 string 타입의 description으로 이루어져요.
+            is_answer로는 true / false boolean의 타입으로 유저가 단어의 뜻을 올바르게 해석했는지 적합성 여부를 판별해주세요. 여러 의미를 가진 단어라면 그 중 하나의 뜻만 제시되어도 정답 처리해주세요. 의미가 비슷하더라도 품사가 많이 다르다면 오답 처리해주세요.
+            description으로는 답안을 판별한 부연설명을 해주세요.`,
           },
         ],
       },
