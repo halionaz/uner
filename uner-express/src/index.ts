c151dadef3ed809cf773bf15b602576a8d7358a1
haionaz <samhsj@naver.com>
2025-01-08 14:14:04 +0900

    feat: Open AI API 아끼기 위해 목업 데이터 넘겨주기

--- uner-express/src/index.ts
+++ uner-express/src/index.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import { GetAnswerRequest, GetWordsRequest } from './types/eng-to-kor';
 import { Word } from './types/word';
+import { APIPromise } from 'openai/core';
 
 dotenv.config();
 
@@ -92,35 +93,49 @@ app.post('/eng-to-kor/answer', (req, res) => {
 
   const prompt = `영어 단어 : ${givenWord}, 답변 : ${userPrompt}`;
 
-  const response = openai.chat.completions.create({
-    model: 'gpt-4o-mini',
-    store: true,
-    messages: [
+  const response = Promise.resolve({
+    choices: [
       {
-        role: 'system',
-        content: [
-          {
-            type: 'text',
-            text: `당신은 한국인 학생을 가르치는 영어 교사에요. 주어진 영어 단어와 비교해서 유저가 제시하는 한국어 단어의 뜻을 판단하고, 적절한 한글 뜻이 제시되었는지 판별해주세요. 
-            답변의 json_object 스키마는 boolean 타입의 is_answer와 string 타입의 description으로 이루어져요.
-            is_answer로는 true / false boolean의 타입으로 유저가 단어의 뜻을 올바르게 해석했는지 적합성 여부를 판별해주세요. 여러 의미를 가진 단어라면 그 중 하나의 뜻만 제시되어도 정답 처리해주세요. 의미가 비슷하더라도 품사가 많이 다르다면 오답 처리해주세요.
-            description으로는 답안을 판별한 부연설명을 해주세요.`,
-          },
-        ],
-      },
-      {
-        role: 'user',
-        content: prompt,
+        message: {
+          content: JSON.stringify({
+            is_answer: true,
+            description: `${userPrompt}는 이렇고 ${givenWord}는 이런데 걍 이래서 답이 맞습니다`,
+          }),
+        },
       },
     ],
-    response_format: {
-      type: 'json_object',
-    },
-    temperature: 0.5,
-    max_completion_tokens: 256,
-    frequency_penalty: 0,
-    presence_penalty: 0,
   });
+
+  // TODO : API 절약
+  // const response = openai.chat.completions.create({
+  //   model: 'gpt-4o-mini',
+  //   store: true,
+  //   messages: [
+  //     {
+  //       role: 'system',
+  //       content: [
+  //         {
+  //           type: 'text',
+  //           text: `당신은 한국인 학생을 가르치는 영어 교사에요. 주어진 영어 단어와 비교해서 유저가 제시하는 한국어 단어의 뜻을 판단하고, 적절한 한글 뜻이 제시되었는지 판별해주세요.
+  //           답변의 json_object 스키마는 boolean 타입의 is_answer와 string 타입의 description으로 이루어져요.
+  //           is_answer로는 true / false boolean의 타입으로 유저가 단어의 뜻을 올바르게 해석했는지 적합성 여부를 판별해주세요. 여러 의미를 가진 단어라면 그 중 하나의 뜻만 제시되어도 정답 처리해주세요. 의미가 비슷하더라도 품사가 많이 다르다면 오답 처리해주세요.
+  //           description으로는 답안을 판별한 부연설명을 해주세요.`,
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       role: 'user',
+  //       content: prompt,
+  //     },
+  //   ],
+  //   response_format: {
+  //     type: 'json_object',
+  //   },
+  //   temperature: 0.5,
+  //   max_completion_tokens: 256,
+  //   frequency_penalty: 0,
+  //   presence_penalty: 0,
+  // });
 
   response.then(result => {
     const answer = result.choices[0].message.content;
