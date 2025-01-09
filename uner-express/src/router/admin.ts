d37dd2d36cce2ee8140f80920003891363bbd691
haionaz <samhsj@naver.com>
2025-01-10 00:24:11 +0900

    feat: /admin API 레이아웃 설계

---
+++ uner-express/src/router/admin.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('words', (req, res) => {});
+router.post('words', (req, res) => {});
+router.delete('words', (req, res) => {});
