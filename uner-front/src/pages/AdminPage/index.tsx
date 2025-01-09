db4b5af3abfe0065c83d8be74d37bff7a1514764
haionaz <samhsj@naver.com>
2025-01-09 11:37:05 +0900

    feat: Admin Page 구현

---
+++ uner-front/src/pages/AdminPage/index.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>uner 어드민 페이지</h1>
+    </div>
+  )
+}
+
+export default AdminPage
--- uner-front/src/router.tsx
+++ uner-front/src/router.tsx
@@ -1,3 +1,4 @@
+import AdminPage from '@/pages/AdminPage'
 import Home from '@/pages/Home'
 import VocabularyPage from '@/pages/VocabularyPage'
 import { RouteObject } from 'react-router-dom'
@@ -5,6 +6,7 @@ import { RouteObject } from 'react-router-dom'
 const routes: RouteObject[] = [
   { path: '/', element: <Home /> },
   { path: '/voca', element: <VocabularyPage /> },
+  { path: '/admin', element: <AdminPage /> },
 ]
 
 export default routes
