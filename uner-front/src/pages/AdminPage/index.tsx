a6ca6958916a7d4790ffda19c164b8fb6ae721af
haionaz <samhsj@naver.com>
2025-01-09 17:39:37 +0900

    feat: Admin Page

--- uner-front/src/pages/AdminPage/index.tsx
+++ uner-front/src/pages/AdminPage/index.tsx
@@ -1,7 +1,10 @@
 const AdminPage = () => {
   return (
-    <div>
-      <h1>uner 어드민 페이지</h1>
+    <div className="h-full w-full flex flex-col items-center gap-4">
+      <h1 className="text-6xl mt-4 font-bold">uner admin</h1>
+      
+      {/* 단어 추가 */}
+      {/* 단어리스트 */}
     </div>
   )
 }
