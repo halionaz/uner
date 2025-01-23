dc03852844e0c6ac51c9600c4d6a81d75b9dfdbe
haionaz <samhsj@naver.com>
2025-01-23 23:30:11 +0900

    feat: TypeORM DB 설정

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { EngWordModule } from '@src/res/engWord/engWord.module';
+import * as path from 'path';
 
 @Module({
-  imports: [EngWordModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10,
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_DB_NAME,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        entities: [path.join(__dirname, 'entities/**/*.entity.{js, ts}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    EngWordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
