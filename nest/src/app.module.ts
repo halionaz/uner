cf494c47ff8109882872084b2d57122774d2fc1f
haionaz <samhsj@naver.com>
2025-01-24 00:01:15 +0900

    fix(bug): include interface 제외

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -13,7 +13,7 @@ import * as path from 'path';
         type: 'mysql',
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT),
-        database: process.env.DB_DB_NAME,
+        database: process.env.DB_NAME,
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         entities: [path.join(__dirname, 'entities/**/*.entity.{js, ts}')],
--- nest/src/data-source.ts
+++ nest/src/data-source.ts
@@ -9,7 +9,7 @@ export const dataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  database: process.env.DB_DB_NAME,
+  database: process.env.DB_NAME,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   entities: [
